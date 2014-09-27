8cec5838ad45958e9227287df09a38e39bc4032e
nikwolfe7 <nikwolfe7@gmail.com>
2014-09-27 00:17:44 -0400

    asdfasdf

--- hw1-nwolfe/src/main/java/SomeClass.java
+++ hw1-nwolfe/src/main/java/SomeClass.java
@@ -1,8 +1,7 @@
 
 public class SomeClass {
   public static void main(String[] args){
-    System.out.println("Hello, world!");
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world!!!!");
   }
 
 }
