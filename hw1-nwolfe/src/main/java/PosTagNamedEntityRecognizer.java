2883b11c68a1d8fc992c9645ec2bc2f232700da2
nikwolfe7 <nikwolfe7@gmail.com>
2014-09-28 22:57:15 -0400

    First commit of archetype...

--- hw1-nwolfe/src/main/java/PosTagNamedEntityRecognizer.java
+++ hw1-nwolfe/src/main/java/PosTagNamedEntityRecognizer.java
@@ -4,19 +4,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.uima.resource.ResourceInitializationException;
+
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.CoreAnnotations.*;
-import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.util.*;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 
 public class PosTagNamedEntityRecognizer {
 
   private StanfordCoreNLP pipeline;
 
-  /**
-   * Constructor
-   */
-  public PosTagNamedEntityRecognizer() {
+  public PosTagNamedEntityRecognizer() throws ResourceInitializationException {
     Properties props = new Properties();
     props.put("annotators", "tokenize, ssplit, pos");
     pipeline = new StanfordCoreNLP(props);
@@ -30,16 +32,21 @@ public class PosTagNamedEntityRecognizer {
     for (CoreMap sentence : sentences) {
       List<CoreLabel> candidate = new ArrayList<CoreLabel>();
       for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-        String pos = token.getString(PartOfSpeechAnnotation.class);
+        String pos = token.get(PartOfSpeechAnnotation.class);
         if (pos.startsWith("NN")) {
           candidate.add(token);
-
         } else if (candidate.size() > 0) {
           int begin = candidate.get(0).beginPosition();
           int end = candidate.get(candidate.size() - 1).endPosition();
           begin2end.put(begin, end);
           candidate.clear();
         }
+      }
+      if (candidate.size() > 0) {
+        int begin = candidate.get(0).beginPosition();
+        int end = candidate.get(candidate.size() - 1).endPosition();
+        begin2end.put(begin, end);
+        candidate.clear();
       }
     }
     return begin2end;
